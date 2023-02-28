5c559358d19f17cfb8ad021e39986c40c1525368
shraddhabidaye <shraddhabidaye@gmail.com>
2023-02-28 06:22:27 +0530

    fixed form list view

--- src/Controller/GeoRedirectController.php
+++ src/Controller/GeoRedirectController.php
@@ -1,29 +1,28 @@
-<?php /**
- * @file
- * Contains \Drupal\geo_redirect\Controller\DefaultController.
- */
+<?php
 
 namespace Drupal\geo_redirect\Controller;
-
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
 
-/**
- * Default controller for the geo_redirect module.
- */
 class GeoRedirectController extends ControllerBase {
 
   public function geo_redirect_list() {
-    $output = '';
+   
+    // return [
+    //   '#markup' => 'hi',
+    // ];
+    // $destination = drupal_get_destination();
 
-    $destination = drupal_get_destination();
-
-    $result = db_select('geo_redirect_list', 'gr')
+    $result = \Drupal::database()
+      ->select('geo_redirect_list', 'gr')
       ->fields('gr', ['gid', 'country_code', 'redirect_url'])
       ->execute();
 
     $rows = [];
     $header = ['Country', 'Redirect URL', 'Operations'];
-    $country_names = geo_redirect_country_names();
+    // $country_names = geo_redirect_country_names();
+    $country_names = "hi";
+
     foreach ($result as $item) {
       $row = [];
       $row[] = ['data' => $country_names[$item->country_code]];
@@ -33,12 +32,12 @@ class GeoRedirectController extends ControllerBase {
       $operations['edit'] = [
         'title' => t('Edit'),
         'href' => 'admin/config/search/geo-redirect/edit/' . $item->gid,
-        'query' => $destination,
+        // 'query' => $destination,
       ];
       $operations['delete'] = [
         'title' => t('Delete'),
         'href' => 'admin/config/search/geo-redirect/delete/' . $item->gid,
-        'query' => $destination,
+        // 'query' => $destination,
       ];
       $row['operations'] = [
         'data' => [
@@ -55,12 +54,24 @@ class GeoRedirectController extends ControllerBase {
         ];
       $rows[] = $row;
     }
-    $output .= theme('table', [
-      'header' => $header,
-      'rows' => $rows,
-      'empty' => t('No geo redirect URLs available.'),
-    ]);
-    return $output;
+    // $output .= theme('table', [
+    //   'header' => $header,
+    //   'rows' => $rows,
+    //   'empty' => t('No geo redirect URLs available.'),
+    // ]);
+    $table = array(
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    );
+    return $table;
+    
   }
-
+  public function geo_redirect_settings() {
+   
+    return [
+      '#markup' => 'hi',
+    ];
+    
+  }
 }
